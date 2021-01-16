a8b19e8715ee8f34bafdd3c5a2ad58c9aa5ac4e4
maliknsj <maliknsj@gmail.com>
2021-01-16 16:23:58 +0500

    added data binding

--- app/src/main/java/com/example/cc_aboutme/MainActivity.kt
+++ app/src/main/java/com/example/cc_aboutme/MainActivity.kt
@@ -8,32 +8,38 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.cc_aboutme.databinding.ActivityMainBinding
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     lateinit var editText:EditText
     lateinit var nickname:TextView
     lateinit var done:Button
+    lateinit var binding:ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViews()
-
-        done.setOnClickListener(){
-            nickname.text=editText.text.toString()
-            nickname.visibility= View.VISIBLE
-            editText.visibility=View.GONE
-            done.visibility=View.GONE
-
+        //setContentView(R.layout.activity_main)
+        //commented because I'm using binding class
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        //findViews()
+        //no need to find views
+        binding.donebtn.setOnClickListener(){
+            binding.apply {
+                nickname.text=binding.editTextTextPersonName.text.toString()
+                invalidateAll()
+                nickname.visibility= View.VISIBLE
+                editTextTextPersonName.visibility=View.GONE
+                donebtn.visibility=View.GONE
+            }
             val imm=getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(it.windowToken,0)
         }
-
     }
-    private fun findViews(){
+    /*private fun findViews(){
         editText=findViewById(R.id.editTextTextPersonName)
         nickname=findViewById(R.id.nickname)
         done=findViewById(R.id.donebtn)
-    }
+    }*/
 }
